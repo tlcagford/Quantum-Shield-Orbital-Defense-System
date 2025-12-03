49718e501794f2d780c3ac1aaac30591c8f60070
Tony E Ford <tlcagford@gmail.com>
2025-12-03 07:35:01 -0700

    Create Machine Learning Enhancement.py

---
+++ Machine Learning Enhancement.py
@@ -0,0 +1,11 @@
+# Add to data_processing/ml/
+class QuantumEnhancedML:
+    """Quantum-classical hybrid ML for threat detection"""
+    
+    def __init__(self):
+        self.qnn = QuantumNeuralNetwork()
+        self.cnn = ClassicalNeuralNetwork()
+        
+    def hybrid_inference(self, sensor_data):
+        # Quantum feature extraction + classical classification
+        pass
